b48ef8c32f4d40877ffe438fe30e19d4f9edc4d8
liubang <it.liubang@gmail.com>
2018-08-22 22:10:44 +0800

    删除重复的代码

--- core/lg-editor.el
+++ core/lg-editor.el
@@ -33,8 +33,6 @@
 ;; set keys for Apple keyboard, for emacs in OS X
 ;; (setq mac-command-modifier 'meta) ; make cmd key do Meta
 ;; (setq mac-option-modifier 'super) ; make opt key do Super
-(setq mac-control-modifier 'control)    ; make Control key do Control
-(setq ns-function-modifier 'hyper)      ; make Fn key do Hyper
 
 (setq
  ;; bookmark
