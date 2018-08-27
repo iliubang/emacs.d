dc9ab5331391ae3795d0628783aa7fe10c635dcf
liubang <it.liubang@gmail.com>
2018-08-27 13:26:42 +0800

    update

--- init.el
+++ init.el
@@ -30,6 +30,12 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; user and email
 (setq user-full-name "liubang")
 (setq user-mail-address "it.liubang@gmail.com")
